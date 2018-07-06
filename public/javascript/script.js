fca17f06d769d9e0d5d2eaa7212bdb1013e08a80
earthmor <earthmor@tuta.io>
2018-07-06 12:48:09 +0300

    add meta tags and fix grammar mistakes

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,15 +1,3 @@
-function openNav() {
-    document.getElementById("js-side_nav").style.width = "250px";
-    document.getElementsByClassName("content")[0].style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
-
-function closeNav() {
-    document.getElementById("js-side_nav").style.width = "0";
-    document.getElementsByClassName("content")[0].style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
-}
-
 (function(){
     document.querySelector('#header > #nav > ul > .icon').addEventListener('click', function(){
         document.querySelector('#header > #nav > ul').classList.toggle('responsive');
